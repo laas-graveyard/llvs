ee855b3ed1a59fd2641f8bc90fc13c0c2a7fe71f
Olivier Stasse <olivier.stasse@aist.go.jp>
2008-10-02 01:00:05 +0000

    Added configuration file.

---
+++ include/llvs/llvsConfig.h.cmake
@@ -0,0 +1,20 @@
+
+#ifndef _LLVS_CONFIG_H_IN_
+#define _LLVS_CONFIG_H_IN_
+
+/* Define to 1 if OpenCV is installed */ 
+#define LLVS_HAVE_OPENCV ${LLVS_HAVE_OPENCV}
+
+/* Define to 1 if VW is installed */ 
+#define LLVS_HAVE_VW ${LLVS_HAVE_VW}
+
+/* Define to 1 if Scene is installed */ 
+#define LLVS_HAVE_SCENE ${LLVS_HAVE_SCENE}
+
+/* Define to 1 if VVV is installed */ 
+#define LLVS_HAVE_VVV ${LLVS_HAVE_VVV}
+
+/* Define to 1 if OmniORB4 is installed */ 
+#define LLVS_HAVE_OMNIORB4 ${LLVS_HAVE_OMNIORB4}
+
+#endif /* _LLVS_CONFIG_H_IN_ */
