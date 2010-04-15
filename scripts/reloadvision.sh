3a89a667550b287da7f8c11fb8663240347d0ade
Clement PETIT <cpetit@jrl001.aist.go.jp>
2010-04-16 03:22:19 +0900

    Enhanced scripts.
    Since /dev/raw1394 is by default in root.disk, adding users
    in this group should simply solve the problem.

--- scripts/reloadvision.sh
+++ scripts/reloadvision.sh
@@ -1,4 +1,5 @@
-#!/bin/bash 
-./unloadvision.sh
-./loadvision.sh
-
+sudo /home/cpetit/bin/unloadvision.sh
+if [ ! $? = 0 ]; then
+ echo "[WARNING] Cannot remove properly modules"
+fi
+sudo /home/cpetit/bin/loadvision.sh
--- scripts/unloadvision.sh
+++ scripts/unloadvision.sh
@@ -1,3 +1,5 @@
+echo "> Unload modules"
+#/sbin/rmmod sbp2
 /sbin/rmmod video1394
 /sbin/rmmod raw1394
 /sbin/rmmod ohci1394
