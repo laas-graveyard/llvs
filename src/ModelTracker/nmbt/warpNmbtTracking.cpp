acf63852fdaa1c50c0c1dc45eee3a20bf98a8d73
Claire Dune <claire.dune@gmail.com>
2010-05-28 10:48:36 +0900

    modif in cmake fils to include the path to model tracker properly

--- src/ModelTracker/nmbt/warpNmbtTracking.cpp
+++ src/ModelTracker/nmbt/warpNmbtTracking.cpp
@@ -35,7 +35,7 @@
    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#include "ModelTracker/nmbtTracking.h"
+#include "ModelTracker/warpNmbtTracking.h"
 #include <iostream>
 
 
