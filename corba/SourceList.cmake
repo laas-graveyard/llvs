8cc70c163d50c75e2336ff23298b6cb9e8cb393c
Clement PETIT <cpetit@jrl001.aist.go.jp>
2010-06-21 18:19:59 +0900

    * Compiling version of LLVS with LLVT

--- corba/SourceList.cmake
+++ corba/SourceList.cmake
@@ -2,7 +2,7 @@ IF(OMNIORB4_FOUND)
 
   SET(IDL_FILES "")
 
-  SET(IDL_INCLUDE_DIR "-I${LLVS_SOURCE_DIR}/corba")
+  SET(IDL_INCLUDE_DIR "${LLVS_SOURCE_DIR}/corba")
  
   #--------------------------------------------------------
   #  Common
@@ -15,10 +15,13 @@ IF(OMNIORB4_FOUND)
   SET(common_CPP  "${LLVS_SOURCE_DIR}/src/commonSK.cc")
   SET(common_Header "${LLVS_SOURCE_DIR}/src/common.hh")
 
-  IDLFILERULE(${COMMON_IDL_NAME}
-              ${common_CPP}
-              ${common_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${COMMON_IDL_NAME}
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${common_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${common_Header})
@@ -32,12 +35,16 @@ IF(OMNIORB4_FOUND)
   SET(BRep_CPP  "${LLVS_SOURCE_DIR}/src/BRepSK.cc")
   SET(BRep_Header "${LLVS_SOURCE_DIR}/src/BRep.hh")
 
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/BRep.idl
-              ${BRep_CPP}
-              ${BRep_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
 
-  SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${BRep_CPP})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/BRep.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
+  
+	SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${BRep_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${BRep_Header})
 
 
@@ -49,11 +56,13 @@ IF(OMNIORB4_FOUND)
   SET(StereoVision_CPP  "${LLVS_SOURCE_DIR}/src/StereoVisionSK.cc")
   SET(StereoVision_Header "${LLVS_SOURCE_DIR}/src/StereoVision.hh")
 
-
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/StereoVision.idl
-              ${StereoVision_CPP}
-              ${StereoVision_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/StereoVision.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${StereoVision_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${StereoVision_Header})
@@ -69,10 +78,13 @@ IF(OMNIORB4_FOUND)
 
   SET(VISIONSENSOR_IDL_NAME ${LLVS_SOURCE_DIR}/corba/visionsensor.idl)
 
-  IDLFILERULE(${VISIONSENSOR_IDL_NAME}
-              ${visionsensor_CPP}
-              ${visionsensor_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${VISIONSENSOR_IDL_NAME}
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${visionsensor_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${visionsensor_Header})
@@ -86,10 +98,13 @@ IF(OMNIORB4_FOUND)
   SET(Scene_CPP  "${LLVS_SOURCE_DIR}/src/SceneSK.cc")
   SET(Scene_Header "${LLVS_SOURCE_DIR}/src/Scene.hh")
 
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/Scene.idl
-              ${Scene_CPP}
-              ${Scene_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/Scene.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${Scene_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${Scene_Header})
@@ -105,10 +120,13 @@ IF(OMNIORB4_FOUND)
   SET(GetGyroAndAccelerometer_CPP  "${LLVS_SOURCE_DIR}/src/GetGyroAndAccelerometerSK.cc")
   SET(GetGyroAndAccelerometer_Header "${LLVS_SOURCE_DIR}/src/GetGyroAndAccelerometer.hh")
 
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/GetGyroAndAccelerometer.idl
-              ${GetGyroAndAccelerometer_CPP}
-              ${GetGyroAndAccelerometer_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/GetGyroAndAccelerometer.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${GetGyroAndAccelerometer_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${GetGyroAndAccelerometer_Header})
@@ -123,10 +141,13 @@ IF(OMNIORB4_FOUND)
   SET(ServerCommand_CPP  "${LLVS_SOURCE_DIR}/src/ServerCommandSK.cc")
   SET(ServerCommand_Header "${LLVS_SOURCE_DIR}/src/ServerCommand.hh")
 
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/ServerCommand.idl
-              ${ServerCommand_CPP}
-              ${ServerCommand_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+  ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/ServerCommand.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${ServerCommand_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${ServerCommand_Header})
@@ -139,10 +160,13 @@ IF(OMNIORB4_FOUND)
   SET(LowLevelVisionSystem_CPP  "${LLVS_SOURCE_DIR}/src/LowLevelVisionSystemSK.cc")
   SET(LowLevelVisionSystem_Header "${LLVS_SOURCE_DIR}/src/LowLevelVisionSystem.hh")
  
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/LowLevelVisionSystem.idl
-              ${LowLevelVisionSystem_CPP}
-              ${LowLevelVisionSystem_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/LowLevelVisionSystem.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${LowLevelVisionSystem_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${LowLevelVisionSystem_Header})
@@ -156,10 +180,13 @@ IF(OMNIORB4_FOUND)
   SET(ModelTrackerInterface_Header "${LLVS_SOURCE_DIR}/src/ModelTrackerInterface.hh")
  
 
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/ModelTrackerInterface.idl
-              ${ModelTrackerInterface_CPP}
-              ${ModelTrackerInterface_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/ModelTrackerInterface.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${ModelTrackerInterface_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${ModelTrackerInterface_Header})
@@ -176,10 +203,13 @@ IF(OMNIORB4_FOUND)
   SET(PointTrackerInterface_Header "${LLVS_SOURCE_DIR}/src/PointTrackerInterface.hh")
  
 
-  IDLFILERULE(${LLVS_SOURCE_DIR}/corba/PointTrackerInterface.idl
-              ${PointTrackerInterface_CPP}
-              ${PointTrackerInterface_Header}
-              ${LLVS_SOURCE_DIR}/src ${IDL_INCLUDE_DIR})
+	 ADD_IDL_FILES(
+        ${LLVS_SOURCE_DIR}/corba/PointTrackerInterface.idl
+        SOURCE_SUFFIX SK.cc
+        HEADER_SUFFIX .hh
+        DESTINATION   ${LLVS_SOURCE_DIR}/src
+        PATHS         ${IDL_INCLUDE_DIR}
+  )
 
   SET(LLVS_SRC_FILES ${LLVS_SRC_FILES} ${PointTrackerInterface_CPP})
   SET(LLVS_HEADER_FILES ${LLVS_HEADER_FILES} ${PointTrackerInterface_Header})
