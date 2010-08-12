652baae5f06998466d8bb2b2f5900c4507d807b3
Claire Dune <claire.dune@gmail.com>
2010-08-13 01:12:11 +0900

    Improve firewire related scripts reliability.
    Old scripts had several issues which made them difficult
    to use.
    The scripts reloadvision.sh could not be called from a
    distant directory, sudo use was imposed to user, etc.
    
    Replace loadvision.sh, unloadvision.sh and reloadvision.sh
    by a self-contained script which is doing the whole work.
    
    loadvision.sh -> modfirewire load
    unloadvision.sh -> modfirewire unload
    reloadvision.sh -> modfirewire reload
    
    The underlying behavior has not changed.
    Also, the new scripts search for module presence before
    loading/unloading to avoid displaying misleading errors.
    Loading or unloading twice does not trigger any errors anymore.
    
    Please note that for module loading/unloading, modfirewire has
    to be run as the root user (by typing ``sudo modfirewire for instance'').
    
    	* scripts/loadvision.sh: Removed.
    	* scripts/modfirewire: New.
    	* scripts/reloadvision.sh: Removed.
    	* scripts/unloadvision.sh: Removed.

--- scripts/reloadvision.sh
+++
@@ -1,5 +0,0 @@
-sudo ./unloadvision.sh
-if [ ! $? = 0 ]; then
- echo "[WARNING] Cannot remove properly modules"
-fi
-sudo ./loadvision.sh
--- scripts/unloadvision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "> Unload modules"
-/sbin/rmmod sbp2
-sudo /sbin/rmmod video1394
-sudo /sbin/rmmod raw1394
-sudo /sbin/rmmod ohci1394
-sudo /sbin/rmmod ieee1394
